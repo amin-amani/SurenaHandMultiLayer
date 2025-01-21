fb293008ef0b1f23412052c1c39fcba0e76587cc
amin-amani <didi1364@gmail.com>
2025-01-21 18:10:46 +0400

    feat: add unity library

---
+++ firmware/test/main_test.c
@@ -0,0 +1,27 @@
+#include "unity.h"
+//#include "../Core/Inc/logic.h"
+#include "string.h"
+#include<unistd.h>
+
+
+
+void test_main_logic()
+{
+
+}
+
+
+void setUp (void)
+{
+
+
+} /* Is run before every test, put unit init calls here. */
+void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_main_logic);
+
+    return UNITY_END();
+}
