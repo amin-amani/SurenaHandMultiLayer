963b0879274c0bd7bf70b3eebcc7b70fc31b4e5e
A.Amani <didi1364@gmail.com>
2025-01-25 22:51:27 +0330

    refactor: seperate pid from motor command

--- firmware/Core/Src/Logic.c
+++ firmware/Core/Src/Logic.c
@@ -5,15 +5,86 @@
  *      Author: amin
  */
 #include "Logic.h"
-
-uint32_t adc_values[6];
-int CurrentPos=0;
-
-int TargetPoition=2048;
-
 void (*set_finger_position)(uint8_t motor,int32_t speed);
 void (*read_adc)(uint32_t* data, uint32_t length);
 void (*delay_ms)(uint32_t delay);
+uint32_t adc_values[6];
+
+typedef struct
+{
+    float kp;
+    float ki;
+    float kd;
+    float i_term;
+    float prev_error;
+    float dt;
+    int max_output;
+    int min_output;
+    int max_command;
+    int min_command;
+    int max_iterm;
+    int min_iterm;
+} pid_element_type;
+
+pid_element_type pid_element[6];
+
+int32_t DoPID(int32_t target_position, int32_t current_position, pid_element_type *pid_elemets)
+{
+    int error = 0;
+    float command;
+    float dterm = 0;
+
+    if (current_position < pid_elemets->min_command || current_position > pid_elemets->max_command)
+    {
+        return 0;
+    }
+
+    error = target_position - current_position;
+    dterm = (error - pid_elemets->prev_error) / pid_elemets->dt;
+
+    pid_elemets->i_term += (error) * pid_elemets->dt;
+
+    if (pid_elemets->i_term > pid_elemets->max_iterm) pid_elemets->i_term = pid_elemets->max_iterm;
+    if (pid_elemets->i_term < pid_elemets->min_iterm) pid_elemets->i_term = pid_elemets->min_iterm;
+
+    command = (pid_elemets->kp * error) + (pid_elemets->kd * dterm) + (pid_elemets->ki * pid_elemets->i_term);
+
+    pid_elemets->prev_error = error;
+
+    if (command > pid_elemets->max_output) command = pid_elemets->max_output;
+    if (command < pid_elemets->min_output) command = pid_elemets->min_output;
+
+    return (int32_t)command;
+}
+
+void logic_start()
+{
+
+pid_element[4].dt=10;
+pid_element[4].kd=0;
+pid_element[4].ki=0;
+pid_element[4].kp=1;
+
+pid_element[4].max_command=3900;
+pid_element[4].min_command=50;
+pid_element[4].max_iterm=1200;
+pid_element[4].min_iterm=-1200;
+pid_element[4].max_output=1900;
+pid_element[4].min_output=-1900;
+
+
+}
+
+void logic_loop()
+{
+	read_adc(adc_values,6);
+	set_finger_position(5, DoPID(1200,adc_values[4],&pid_element[4]));
+	delay_ms(10);
+
+}
+
+
+
 
 void register_delay(void (*delay_callback)(uint32_t delay))
 {
@@ -29,62 +100,10 @@ void register_adc_callback(void (*read_adc_callback)(uint32_t* data, uint32_t le
 	read_adc=read_adc_callback;
 }
 
-void DoPID()
-{
-	float ki=0.00,kp=1,kd=0.00;
-
-	int error=0;
-	int intcmd;
-	int dt=10;
-	static int prevError=0;
-	float command=1200;
-	float Dterm=0;
-	static float Iterm=0;
-
-	CurrentPos=adc_values[4];//UpdateEncoder();
-
-	if(CurrentPos<50 || CurrentPos>3900 )
-	{
-		set_finger_position(7, 0);
-		return;
-	}
-
-//	printf("adc5=%d\n",CurrentPos);
-	error=TargetPoition-CurrentPos;
-	Dterm=(error-prevError)/dt;
-	Iterm+=(error)*dt;
-	if(Iterm>1200)Iterm=1200;
-	if(Iterm<-1200)Iterm=-1200;
-
-	command=kp*error+kd*Dterm+ki*Iterm;
-
-	prevError=error;
-		if(command>1900)command=1900;
-		if(command<-1900)	command=-1900;
-		intcmd=command;
-
-	 delay_ms(10);
-	//threshold
-//	if(command<300)command=300;
-//	if(command>2300)command=2300;
-
-		set_finger_position(5, command);
 
 
-}
 void can_data_received(uint32_t id,uint8_t*data)
 {
 
 }
-void logic_start()
-{
 
-
-}
-void logic_loop()
-{
-	read_adc(adc_values,6);
-
-//	DoPID();
-//	 set_finger_position(5,-1900);
-}
--- firmware/test/main_test.c
+++ firmware/test/main_test.c
@@ -3,7 +3,12 @@
 #include "string.h"
 #include<unistd.h>
 
+void read_adc_mock(uint32_t * data, uint32_t length)
+{
+    uint16_t ADCResult[6]={1,2,3,4,5,6};
+    memcpy(data,ADCResult,length);
 
+}
 
 void test_main_logic()
 {
@@ -13,7 +18,7 @@ void test_main_logic()
 
 void setUp (void)
 {
-
+    register_adc_callback(read_adc_mock);
 
 } /* Is run before every test, put unit init calls here. */
 void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
@@ -22,6 +27,7 @@ int main()
     UNITY_BEGIN();
 
     RUN_TEST(test_main_logic);
+
 
     return UNITY_END();
 }
